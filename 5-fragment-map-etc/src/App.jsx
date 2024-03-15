05fd8b60fa121994d6a9fec6dc8b17bfce90bc0f
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-03-15 09:13:46 +0530

    map-used-instead-of-for-loop-in-react

--- 5-fragment-map-etc/src/App.jsx
+++ 5-fragment-map-etc/src/App.jsx
@@ -1,15 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
+  let foodIteams = ["Dal","Green Vegetable","Roti","Salad","Mild"];
 
   return (
     <>
       <h1>Healthy Foods</h1>
       <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        {foodIteams.map(item=> <li class="list-group-item">{item}</li>)}
       </ul>
     </>
   )
