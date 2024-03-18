1911ec2d6fc0d2f48e1e93db48157c0b0b23f6ea
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-03-18 08:31:06 +0530

    made-dyanmic-using-hooks

--- 7-State-Management-implimentation/src/components/FoodInput.jsx
+++ 7-State-Management-implimentation/src/components/FoodInput.jsx
@@ -1,14 +1,14 @@
 import styles from "./FoodInput.module.css";
 
-const FoodInput = (props) =>{
+const FoodInput = (props) => {
 
 
     return (
         <input type="text"
             placeholder="Enter Food Iteam"
             className={styles.foodInput}
-            onChange={props.handleOnChange}
-            >
+            onKeyDown={props.onKeyDown}
+        >
         </input>
     );
 };
--- 7-State-Management-implimentation/src/components/FoodIteams.jsx
+++ 7-State-Management-implimentation/src/components/FoodIteams.jsx
@@ -2,22 +2,19 @@ import { useState } from "react";
 import FoodInput from "./FoodInput";
 import Item from "./Item";
 const FoodIteams = () => {
-    // let foodIteams = [];
-    let foodIteams = ["Dal", "Green Vegetable", "Roti", "Salad", "Mild"];
-    // let textStateArr = useState("Food Iteam ented by user"); // by default return array of 2 length
-    // let textToShow = textStateArr[0]; // first value of state
-    // let setTextState = textStateArr[1]; // second function to set valye of state
+    // let [foodIteams, setFoodIteams] = useState(["Dal", "Green Vegetable"]);
+    let [foodIteams, setFoodIteams] = useState([]);
+    const onKeyDown = (event) => {
+        if (event.key === "Enter") {
+            let newFoodItem = event.target.value;
+            let newIteams = [...foodIteams, newFoodItem];
+            setFoodIteams(newIteams);
+        }
 
-    let [textToShow, setTextState] = useState("Food Iteam ented by user"); // concise way
-    console.log(`data eneter by user i ${textToShow}`);
-    const handleOnChange = (event) => {
-        console.log(event.target.value);
-        setTextState(event.target.value); // set state value
     }
     return (
-        <> 
-            <FoodInput handleOnChange={handleOnChange} />
-            <p>{textToShow}</p>
+        <>
+            <FoodInput onKeyDown={onKeyDown} />
             <ul className="list-group">
                 {foodIteams.map((items) => {
                     return <Item key={items} foodItem={items}></Item>
