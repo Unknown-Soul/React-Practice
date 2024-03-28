d4551d41c4e4be6bfe803ff7ece62b0f5360584a
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-03-28 10:26:01 +0530

    basic-context-api-done

---
+++ 8-app-todo-version-3-ContextAPI/src/components/AddTodo.jsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import styles from "./AddTodo.module.css"
+import { IoIosAdd } from "react-icons/io";
+
+function AddTodo({ onAddTodoIteam }) {
+    const task = useRef();
+    const date = useRef();
+
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const taskTemp = task.current.value;
+        const dateTemp =  date.current.value
+        onAddTodoIteam(taskTemp,dateTemp);
+        task.current.value = "";
+        date.current.value = "";
+    };
+    return <div className="container">
+        <form className={`row ${styles.customRow}`}
+            onSubmit={handleAddTodo}
+        >
+            <div className="col-6">
+                <input type="text" placeholder='Enter Todo'  ref={task}/></div>
+            <div className="col-4">
+                <input type="date" name="datePicker" id="date" ref={date}/></div>
+            <div className="col-2">
+                <button type='submit' className={`btn btn-success ${styles.customButton}`}><IoIosAdd /></button>      </div>
+        </form>
+    </div>
+        ;
+}
+
+export default AddTodo;
