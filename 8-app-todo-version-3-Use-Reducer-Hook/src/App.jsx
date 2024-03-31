557d9a60427323b984fa56be02fded8e6bb4e042
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-03-31 10:40:17 +0530

    clean-up-App-Code-Moved-to-Store-Better-Sepration

--- 8-app-todo-version-3-Use-Reducer-Hook/src/App.jsx
+++ 8-app-todo-version-3-Use-Reducer-Hook/src/App.jsx
@@ -2,58 +2,14 @@ import AppName from './components/AppName.'
 import AddTodo from './components/AddTodo'
 import "./App.css";
 import TodoIteams from './components/TodoIteams';
-import { useReducer } from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
-import { TodoItemsContext } from './store/todo-iteam-store';
-
-const TodoIteamsReducer = (currentStateOfTodo, action) => {
-  let newTodoItems = currentStateOfTodo;
-  if (action.type === "ADD_NEW_ITEAM") {
-    newTodoItems = [
-      ...currentStateOfTodo,
-      {
-        itemName: action.payload.itemName,
-        date: action.payload.date,
-      },
-    ];
-  } else if (action.type === "DELETE_ITEAM") {
-    newTodoItems = currentStateOfTodo.filter((item) => item.itemName !== action.payload.itemName)
-  }
-  return newTodoItems;
-}
+import TodoIteamContextProvider from './store/todo-iteam-store';
 
 function App() {
-  const [todoItemList, dispatchTodoIteams] = useReducer(TodoIteamsReducer, []);
 
-  function onAddTodoIteam(item, date) {
-    const newItemAction = {
-      type: "ADD_NEW_ITEAM",
-      payload: {
-        itemName: item,
-        date: date,
-      }
-    }
-    dispatchTodoIteams(newItemAction);
-  };
-
-
-  function onDeleteTodoIteam(itemNameToDelete) {
-    const newItemAction = {
-      type: "DELETE_ITEAM",
-      payload: {
-        itemName: item,
-      }
-    };
-    dispatchTodoIteams(newItemAction);
-  };
 
   return (
-    <TodoItemsContext.Provider value={{
-      items: todoItemList,
-      addIteams: onAddTodoIteam,
-      deleteIteams: onDeleteTodoIteam,
-    }
-    }>
+    <TodoIteamContextProvider>
       <center className='todoAppContainer'>
         <AppName />
         <div className='item-container'>
@@ -62,7 +18,7 @@ function App() {
           <TodoIteams />
         </div>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoIteamContextProvider>
   );
 }
 
--- 8-app-todo-version-3-Use-Reducer-Hook/src/store/todo-iteam-store.jsx
+++ 8-app-todo-version-3-Use-Reducer-Hook/src/store/todo-iteam-store.jsx
@@ -1,9 +1,60 @@
 import { createContext } from "react";
 
-const TodoItemsContext = createContext({
+export const TodoItemsContext = createContext({
     items: [],
     addIteams: () => { },
     deleteIteams: () => { },
 });
 
-export { TodoItemsContext };
+const TodoIteamsReducer = (currentStateOfTodo, action) => {
+    let newTodoItems = currentStateOfTodo;
+    if (action.type === "ADD_NEW_ITEAM") {
+        newTodoItems = [
+            ...currentStateOfTodo,
+            {
+                itemName: action.payload.itemName,
+                date: action.payload.date,
+            },
+        ];
+    } else if (action.type === "DELETE_ITEAM") {
+        newTodoItems = currentStateOfTodo.filter((item) => item.itemName !== action.payload.itemName)
+    }
+    return newTodoItems;
+}
+
+
+// component
+const TodoIteamContextProvider = ({ children }) => {
+    const [todoItemList, dispatchTodoIteams] = useReducer(TodoIteamsReducer, []);
+
+    function onAddTodoIteam(item, date) {
+        const newItemAction = {
+            type: "ADD_NEW_ITEAM",
+            payload: {
+                itemName: item,
+                date: date,
+            }
+        }
+        dispatchTodoIteams(newItemAction);
+    };
+
+
+    function onDeleteTodoIteam(itemNameToDelete) {
+        const newItemAction = {
+            type: "DELETE_ITEAM",
+            payload: {
+                itemName: item,
+            }
+        };
+        dispatchTodoIteams(newItemAction);
+    };
+    return (<TodoItemsContext.Provider value={{
+        items: todoItemList,
+        addIteams: onAddTodoIteam,
+        deleteIteams: onDeleteTodoIteam,
+    }
+    }>{children}</TodoItemsContext.Provider>
+    );
+};
+
+export default TodoIteamContextProvider;
