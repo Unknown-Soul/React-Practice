1130229d5fc6a96cd516541f80d5d9168e9171ad
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-04-01 08:50:29 +0530

    basic-struture-added

---
+++ 9-social-media/src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+function App() {
+
+  return (
+      <div className="app-continer">
+        <SideBar></SideBar>
+        <div className="content">
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+      </div>
+  );
+}
+
+export default App;
