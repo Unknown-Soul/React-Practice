972e867d0a7302d207b7f0de2088d0c141d3bad4
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-04-03 09:02:09 +0530

    project9-social-media-basic-structure-defined

--- 9-social-media/src/App.jsx
+++ 9-social-media/src/App.jsx
@@ -3,16 +3,26 @@ import './App.css'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SideBar from "./components/SideBar";
+import CreatePost from "./components/CreatPost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
 
   return (
-      <div className="app-continer">
-        <SideBar></SideBar>
-        <div className="content">
-            <Header></Header>
-            <Footer></Footer>
-        </div>
+    <div className="app-continer">
+      <SideBar selectedTab={selectedTab}></SideBar>
+      <div className="content">
+        <Header></Header>
+        {selectedTab === "Home" ? (
+          <PostList></PostList>
+        ) : (
+          <CreatePost></CreatePost>
+        )}
+        <Footer></Footer>
       </div>
+    </div>
   );
 }
 
