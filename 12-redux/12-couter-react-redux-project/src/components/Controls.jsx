6bce2c9ad4de60a65943607d1b11bc26859b9472
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-04-26 06:23:14 +0530

    redux-dispactch-done

--- 12-redux/12-couter-react-redux-project/src/components/Controls.jsx
+++ 12-redux/12-couter-react-redux-project/src/components/Controls.jsx
@@ -1,8 +1,40 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+
 const Controls = () => {
-    return <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">+1</button>
-        <button type="button" className="btn btn-outline-secondary btn-lg px-4">-1</button>
-    </div>
+
+    const inputElement = useRef();
+    const dispatch = useDispatch();
+    const handleDecrement = () => {
+        dispatch({ type: "DECREMENT" });
+    }
+
+    const handleIncreamnt = () => {
+        dispatch({ type: "INCREMENT" });
+    }
+
+
+    const handleAdd = () => {
+        dispatch({ type: "ADD", payload: { num: inputElement.current.value } });
+        inputElement.current.value = "";
+    }
+
+    const handleSubtract = () => {
+        dispatch({ type: "SUBTRACT", payload: { num: inputElement.current.value } });
+        inputElement.current.value = "";
+    }
+
+    return <>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3" onClick={handleIncreamnt}>+1</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg px-4" onClick={handleDecrement}>-1</button>
+        </div>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+            <input type="text" placeholder="Enter Number" className="number-input" ref={inputElement}></input>
+            <button type="button" className="btn btn-info" onClick={handleAdd}>Add</button>
+            <button type="button" className="btn btn-danger" onClick={handleSubtract}>Subtract</button>
+        </div>
+    </>
 }
 
 export default Controls;
--- 12-redux/12-couter-react-redux-project/src/store/index.js
+++ 12-redux/12-couter-react-redux-project/src/store/index.js
@@ -5,6 +5,15 @@ const INTITIAL_VALUE = {
 }
 // reducer
 const counterReducer = (store = INTITIAL_VALUE, action) => {
+    console.log("Action Recived", action);
+    if (action.type === 'DECREMENT')
+        return { counter: store.counter - 1 };
+    if (action.type === 'INCREMENT')
+        return { counter: store.counter + 1 };
+    if (action.type === 'ADD')
+        return { counter: store.counter + Number(action.payload.num) };
+    if (action.type === 'SUBTRACT')
+        return { counter: store.counter - Number(action.payload.num) };
     return store;
 }
 
