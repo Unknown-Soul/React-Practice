882ce2a68feecfa55108960c1d764fd45fe6786c
Unknown-Soul <ashutoshgupta1231@gmail.com>
2024-05-06 06:58:17 +0530

    myntra-loading-component-done-

---
+++ 13-mytra-clone/myntra-react-clone/src/components/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+    return <center className="spinner">
+        <div className="spinner-border" role="status">
+        </div>
+    </center>
+}
+
+export default Loading;
--- 13-mytra-clone/myntra-react-clone/src/routes/App.jsx
+++ 13-mytra-clone/myntra-react-clone/src/routes/App.jsx
@@ -2,11 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import FetchItem from '../components/FetchItem';
+import Loading from '../components/Loading';
+import { useSelector } from 'react-redux';
 function App() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
   return <>
     <Header/>
     <FetchItem/>
-    <Outlet />
+     {fetchStatus.currentlyFetching ? <Loading/>: <Outlet/>}
+    {/* <Outlet /> */}
     <Footer />
   </>
 }
